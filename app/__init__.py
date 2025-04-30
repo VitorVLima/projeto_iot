0efeedcd65e2832585bdd151e0a880cb354d930f
VitorVLima <vitor14_df@hotmail.com>
2025-04-30 14:51:39 -0300

    novaatt

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_manager, LoginManager
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+
+
 
 
 app = Flask(__name__)
@@ -9,6 +12,7 @@ app.config['SECRET_KEY'] = 'SUFNPSDNU389-90CDJI0JQWOX'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+CORS(app)
 
 #configurações de Login
 login_manager = LoginManager()
