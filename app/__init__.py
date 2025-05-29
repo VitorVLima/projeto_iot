5a6fe315a23a5d2cd86a340592cea47369d7e081
VitorVLima <vitor14_df@hotmail.com>
2025-05-29 16:17:25 -0300

    arr

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'SUFNPSDNU389-90CDJI0JQWOX'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Vi076466@iot-app.ctyss6u6e8fw.us-east-2.rds.amazonaws.com:3306/iot_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 #.u=Hyu!uLj+s.48
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
